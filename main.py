43e3fc489808b13d412c52d03fbf3251f0a6f199
ShatskikhA <shatskikh.andrey.2008@gmail.com>
2024-11-30 15:25:16 +0300

    ver2

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QWidget
-from PyQt6.QtGui import QPainter
+from PyQt6.QtGui import QPainter, QColor
 from PyQt6.QtCore import QPoint
 from PyQt6 import uic
+from random import randint
 
 
 class Circle(QWidget):
@@ -12,12 +13,22 @@ class Circle(QWidget):
         self.pushButton.clicked.connect(self.run)
         self.do_paint = False
 
+    def paintEvent(self, event):
+        qp = QPainter
+        qp.begin(self)
+        self.paint_ring(qp)
+        qp.end()
+
+    def paint_ring(self, qp):
+        qp.setPen(QColor())
+
+
     def run(self):
         self.do_paint = True
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Circle
+    ex = Circle()
     ex.show()
     sys.exit(app.exec())
